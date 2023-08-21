f5d77f107a5e6b09fa90838ffde1a8210b37e509
Yuvraj Chandra <Singhyuvraj179@gmail.com>
2023-08-21 14:56:01 +0530

    fixed code formatting

--- script.js
+++ script.js
@@ -13,7 +13,15 @@ let refreshButton = document.querySelector('#refreshButton');
 
 
 // alphaNums contains the characters with which you want to create the CAPTCHA
-let alphaNums = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+let alphaNums = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 
+                'H', 'I', 'J', 'K', 'L', 'M', 'N', 
+                'O', 'P', 'Q', 'R', 'S', 'T', 'U', 
+                'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 
+                'c', 'd', 'e', 'f', 'g', 'h', 'i', 
+                'j', 'k', 'l', 'm', 'n', 'o', 'p', 
+                'q', 'r', 's', 't', 'u', 'v', 'w', 
+                'x', 'y', 'z', '0', '1', '2', '3', 
+                '4', '5', '6', '7', '8', '9'];
 let emptyArr = [];
 
 // This loop generates a random string of 7 characters using alphaNums
